a62035781d2b63aad7e4d166e4617d6338e9e236
Zengjian Hu <zengjian.hu@relational.ai>
2023-11-03 10:47:11 -0700

    remove extra info and comments

--- test/all_tests.jl
+++ test/all_tests.jl
@@ -81,8 +81,6 @@ end
     # solve and check output is expected for each input json file
     @testset "$j" for j in json_names
         output = JSON3.read(run_solve(read_json("inputs", j)))
-        @info output
-        #isdefined(Main, :Infiltrator) && Main.infiltrate(@__MODULE__, Base.@locals, @__FILE__, @__LINE__)
         @test output.solver_version isa String
         @test output.solve_time_sec isa Float64
         expect = JSON3.read(read_json("outputs", j))
