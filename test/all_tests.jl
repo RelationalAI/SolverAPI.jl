50336c147e286a702a2235fd8a21bb6ad2f087ed
David Bach <david.bach@relational.ai>
2023-11-22 12:38:07 +0200

    debug: add expect log

--- test/all_tests.jl
+++ test/all_tests.jl
@@ -83,10 +83,11 @@ end
     # solve and check output is expected for each input json file
     @testset "$j" for j in json_names
         output = JSON3.read(run_solve(read_json("inputs", j)))
-        @info output
+        @info "output" output
         @test output.solver_version isa String
         @test output.solve_time_sec isa Float64
         expect = JSON3.read(read_json("outputs", j))
+        @info "expect" expect
         for (key, expect_value) in pairs(expect)
             @test output[key] == expect_value
         end
